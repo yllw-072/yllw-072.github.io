6df158ee35f0b405f075ed6f70afe4748474d31b
yllw-072 <55445135+yllw-072@users.noreply.github.com>
2019-10-03 08:35:33 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('
+		../sw.js')
+	.then((reg) => console.log('
+		Service worker registrado'))
+	.catch((err) => console.log('
+		Service worker não registrado' , err));
+}
