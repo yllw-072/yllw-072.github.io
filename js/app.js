4589843a1ed98a9cb176f4a450aa38e18c861f11
yllw-072 <55445135+yllw-072@users.noreply.github.com>
2019-10-03 08:47:16 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('
-		../sw.js')
-	.then((reg) => console.log('
-		Service worker registrado'))
-	.catch((err) => console.log('
-		Service worker não registrado' , err));
+	navigator.serviceWorker.register('../sw.js')
+	.then((reg) => console.log('Service worker registrado'))
+	.catch((err) => console.log('Service worker não registrado' , err));
 }
